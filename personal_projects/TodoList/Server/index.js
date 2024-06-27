b2e7ca81bb8f5ad33905ec1fa7da45f214e849b0
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-06-28 00:50:29 +0200

    connecting database to the backend

--- personal_projects/TodoList/Server/index.js
+++ personal_projects/TodoList/Server/index.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect('mongodb://127.0.0.1:27017/');
+
+app.post('/add', (req, res => {
+  const task = req.body.task;
+}))
 app.listen(3001, () => {
   console.log("Server is running")
 })
