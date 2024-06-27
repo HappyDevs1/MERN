d7c24bfbec4e7c19dcdadaf396f996a2d687f7d3
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-06-27 18:54:52 +0200

    allowing users to interact with reviews

--- book_1/chapter_10/movie-reviews/backend/index.js
+++ book_1/chapter_10/movie-reviews/backend/index.js
@@ -1,7 +1,9 @@
 import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
-import MoviesDAO from './dao/moviesDAO.js'  
+import MoviesDAO from "./dao/moviesDAO.js";
+import ReviewsDAO from "./dao/reviewsDAO.js";
+
 async function main() {
   dotenv.config();
   const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI);
@@ -9,7 +11,8 @@ async function main() {
   try {
     // Connect to the MongoDB cluster
     await client.connect();
-    await MoviesDAO.injectDB(client) 
+    await MoviesDAO.injectDB(client);
+    await ReviewsDAO.injectDB(client);
     app.listen(port, () => {
       console.log("server is running on port:" + port);
     });
@@ -18,4 +21,5 @@ async function main() {
     process.exit(1);
   }
 }
+
 main().catch(console.error);
