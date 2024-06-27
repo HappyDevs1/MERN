b114fcadbcbc6478ff7969cb583ab045d2ae9be0
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-06-27 22:16:13 +0200

    adding controller to get movies by id and by ratings

--- book_1/chapter_10/movie-reviews/backend/api/movies.route.js
+++ book_1/chapter_10/movie-reviews/backend/api/movies.route.js
@@ -4,6 +4,8 @@ const router = express.Router(); // get access to express router
 import ReviewsController from "./reviews.controller.js";
 
 router.route("/").get(MoviesController.apiGetMovies);
+router.route("/id/:id").get(MoviesController.apiGetMovieById) 
+router.route("/ratings").get(MoviesController.apiGetRatings)
 router
   .route("/review")
   .post(ReviewsController.apiPostReview)
