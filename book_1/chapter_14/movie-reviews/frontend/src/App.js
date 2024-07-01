e99513c6f2e64d43cfe3bec129cb07d8cefb4d62
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-07-01 20:32:23 +0200

    importing components into the main component

--- book_1/chapter_14/movie-reviews/frontend/src/App.js
+++ book_1/chapter_14/movie-reviews/frontend/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import addReview from './components/add-review';
+import MovieList from './components/movies-list';
+import Movie from './components/movie';
+import Login from './components/login';
 
 function App() {
   return (
