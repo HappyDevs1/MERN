8641e30e90d1586d9906df6ad5944752f88a1ba9
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-07-04 23:47:12 +0200

    editing reviews

--- book_1/chapter_21/movie-reviews/frontend/src/components/add-review.js
+++ book_1/chapter_21/movie-reviews/frontend/src/components/add-review.js
@@ -7,10 +7,16 @@ import Button from "react-bootstrap/Button";
 const AddReview = (props) => {
   let editing = false;
   let initialReviewState = "";
+
+  if (props.location.state && props.location.state.currentReview) {
+    editing = true;
+    initialReviewState = props.location.state.currentReview.review;
+  }
+
   const [review, setReview] = useState(initialReviewState);
   // keeps track if review is submitted
   const [submitted, setSubmitted] = useState(false);
-  
+
   const onChangeReview = (e) => {
     const review = e.target.value;
     setReview(review);
@@ -22,13 +28,27 @@ const AddReview = (props) => {
       user_id: props.user.id,
       movie_id: props.match.params.id, // get movie id direct from url
     };
-    MovieDataService.createReview(data)
-      .then((response) => {
-        setSubmitted(true);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
+
+    if (editing) {
+      // get existing review id
+      data.review_id = props.location.state.currentReview._id;
+      MovieDataService.updateReview(data)
+        .then((response) => {
+          setSubmitted(true);
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    } else {
+      MovieDataService.createReview(data)
+        .then((response) => {
+          setSubmitted(true);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
   };
   return (
     <div>
