bbb75ff3dc7dd78ad7564647dda56466c48ffaf7
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-07-07 23:49:21 +0200

    creating book model with mongoose

--- tutorial_1/backend/index.js
+++ tutorial_1/backend/index.js
@@ -9,14 +9,13 @@ app.get("/", (request, response) => {
   return response.status(234).send("Welcome to MERN Stack Tutorial")
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
-});
-
 mongoose
   .connect(mongoDBURL)
   .then(() => {
     console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
   })
   .catch((error) => {
     console.log(error);
