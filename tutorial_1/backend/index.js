93ddae69b2019b75d029f82764319f1090c127ab
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-07-08 22:55:23 +0200

    implementing cors in my application

--- tutorial_1/backend/index.js
+++ tutorial_1/backend/index.js
@@ -3,11 +3,17 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRouter from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use("/books", booksRouter);
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
 
 app.get("/", (request, response) => {
   console.log(request);
