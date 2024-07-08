beb18bbd34e29c4e992004d17dc8cea763633b20
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-07-08 06:51:08 +0200

    adding schema and post request to create books

--- tutorial_1/backend/index.js
+++ tutorial_1/backend/index.js
@@ -1,13 +1,44 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to MERN Stack Tutorial")
 });
+
+app.post(("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear"
+      })
+    }
+
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear
+    };
+
+    const book = await book.create(newbook);
+
+    return response.status(201).send(book);
+
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send.json({ message: error.message });
+  }
+}))
 
 mongoose
   .connect(mongoDBURL)
