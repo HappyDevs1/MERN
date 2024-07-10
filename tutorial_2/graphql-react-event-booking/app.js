d6fbc278d5a2ed7fc19a217cbf76c7c930d03453
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-07-10 21:49:09 +0200

    adding the creator of the event and connecting the creator of the event and the user who created the event

--- tutorial_2/graphql-react-event-booking/app.js
+++ tutorial_2/graphql-react-event-booking/app.js
@@ -74,12 +74,24 @@ app.use(
           description: args.eventInput.description,
           price: +args.eventInput.price,
           date: new Date(args.eventInput.date),
+          creator: '668ee0c5ed1ba1dee223db14'
         });
+        let createdEvent;
         return event
           .save()
           .then((result) => {
-            console.log(result);
-            return { ...result._doc, _id: result._doc._id.toString() };
+            createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+            return User.findById('668ee0c5ed1ba1dee223db14');
+          })
+          .then(user => {
+            if (!user) {
+              throw new Error('User not found.')
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then(result => {
+            return createdEvent;
           })
           .catch((error) => {
             console.log(error);
@@ -87,8 +99,13 @@ app.use(
           });
       },
       createUser: (args) => {
-        return bcrypt
+        return User.findOne({email: args.userInput.email}).then(user => {
+          if (user) {
+            throw new Error('User exists already.')
+          }
+          return bcrypt
           .hash(args.userInput.password, 12)
+        })
           .then((hashedPassword) => {
             const user = new User({
               email: args.userInput.email,
@@ -97,7 +114,7 @@ app.use(
             return user.save();
           })
           .then(result => {
-            return { ...result._doc, _id: result.id }
+            return { ...result._doc, password: null, _id: result.id }
           })
           .catch((error) => {
             throw error;
